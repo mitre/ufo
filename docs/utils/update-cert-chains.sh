fdeda0f09f1570e4801ffa9e7ebaa7fae0f3e67f
Aaron Lippold <lippold@gmail.com>
2019-09-28 15:59:03 -0400

    added note about the AWS_CA_BUNDLE env_var

--- docs/utils/update-cert-chains.sh
+++ docs/utils/update-cert-chains.sh
@@ -5,7 +5,10 @@ echo 'What is the uri to your organizations root certificate chain?'
 read -p 'org_root_chain: ' org_root_chain
 echo "$org_root_chain"
 curl "$org_root_chain" -o org_chain.txt
+echo "Your certificate chain is located at: $cert_file"
 cat org_chain.txt >> "$cert_file"
 mkdir -p "${cert_file%/*}"
 security find-certificate -a -p /Library/Keychains/System.keychain > "$cert_file"
 security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain >> "$cert_file"
+export AWS_CA_BUNDLE="$cert_file"
+echo "NOTE: You should add export AWS_CA_BUNDLE=$cert_file to your .bash_profile or .zshrc"
